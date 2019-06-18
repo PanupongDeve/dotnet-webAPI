caac55021414dcaf901d2c00fbd5115e83e22b2c
Panupong <5635512110@psu.ac.th>
2019-06-18 09:39:30 +0700

    Add AuthReposity

--- DemoApp.API/Migrations/20190618015056_AddedUserEntity.Designer.cs
+++ DemoApp.API/Migrations/20190618015056_AddedUserEntity.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DemoApp.API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DemoApp.API.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20190618015056_AddedUserEntity")]
+    partial class AddedUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -21,11 +23,11 @@ namespace DemoApp.API.Migrations
                     b.Property<int>("Id")
                         .ValueGeneratedOnAdd();
 
-                    b.Property<byte[]>("PasswordHash");
+                    b.Property<string>("UUsername");
 
-                    b.Property<byte[]>("PasswordSalt");
+                    b.Property<byte[]>("passwordHash");
 
-                    b.Property<string>("Username");
+                    b.Property<byte[]>("passwordSalt");
 
                     b.HasKey("Id");
 
