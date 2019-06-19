ca8c293fdd159d50603dc22cf2b677cf2279cb08
Panupong <5635512110@psu.ac.th>
2019-06-19 14:59:03 +0700

    add alertify 3rd-party  components

--- DemoApp-SPA/src/app/nav/nav.component.ts
+++ DemoApp-SPA/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,16 +11,16 @@ export class NavComponent implements OnInit {
   title = 'DemoApp-SPA';
   model:any = {};
 
-  constructor(private authServices: AuthService) { }
+  constructor(private authServices: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   login() {
     this.authServices.login(this.model).subscribe(next => {
-      console.log('Logged is successfully');
+      this.alertify.success('logged in successfullty')
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
@@ -30,6 +31,6 @@ export class NavComponent implements OnInit {
 
   logout() {
     localStorage.removeItem('token');
-    console.log('logged out');
+    this.alertify.message('logged out');
   }
 }
