aa154be9f587422c689586931e3291e2dad4e23f
Panupong <5635512110@psu.ac.th>
2019-06-28 15:23:15 +0700

    create fontend interface

--- DemoApp.API/Dtos/UserForLoginDto.cs
+++ DemoApp.API/Dtos/UserForLoginDto.cs
@@ -8,7 +8,7 @@ namespace DemoApp.API.Dtos
         public string Username { get; set; }
 
         [Required]
-        [StringLength(8, MinimumLength = 4, ErrorMessage="you must specify between 4 and 8")]
+        [StringLength(20, ErrorMessage="you must specify between 4 and 8", MinimumLength = 4)]
         public string Password{ get; set; }
     }
 }
